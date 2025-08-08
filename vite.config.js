a28cf01e1cef4357803d7a2a9d602288fb391b41
Dhruvil Gautam <dhruvilgautam5@gmail.com>
2025-08-08 13:11:28 +0530

    Your descriptive commit message

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
-import monacoEditorPlugin from 'vite-plugin-monaco-editor';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), monacoEditorPlugin()],
+  plugins: [react()],
 })
