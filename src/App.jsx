2c3379ed6cf5cd406c4ef6ebdd0f459a2f114386
Dhruvil Gautam <dhruvilgautam5@gmail.com>
2025-11-26 00:50:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
   const [code, setCode] = useState(""); // State to store user-entered code
 
   const ai = new GoogleGenAI({
-    apiKey: "AIzaSyBEcywnKwCK5i8ks_MseSiVdttonSgfk4c", // Google GenAI API key
+    apiKey: "AIzaSyAnepeXhsvD8iDLZhPtBp-yn-iA7oizN-w", // Google GenAI API key
   });
 
   const [loading, setLoading] = useState(false); // State for loader
